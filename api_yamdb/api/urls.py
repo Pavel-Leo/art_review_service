0e8931d6b5a582181a2593c5fe270f744921bea0
Анатолий Шабанов <Ulfa1986@gmail.com>
2023-05-09 13:21:44 +0300

    some fixes setup.cfg, requirements.txt

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from api.views import CommentViewSet, TitleViewSet
+from api.views import (CommentViewSet, TitleViewSet, GenreViewSet,
+                       CategoryViewSet, ReviewViewSet)
 
 router = SimpleRouter()
 router.register('comments', CommentViewSet, basename='comments')
+router.register('titles', TitleViewSet, basename='titles')
+router.register('genres', GenreViewSet, basename='genres')
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('reviews', ReviewViewSet, basename='reviews')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, viewsets
 
-# Create your views here.
+from api.serializers import (
+    CommentSerializer,
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+    ReviewSerializer,
+)
+
+from core.models import Comment, Category, Genre, Title, Review
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
