9ef40211fe06297440c6b3fb03edeffb4f9752b3
yvk3 <yvk3@mail.ru>
2023-05-12 21:30:26 +0300

    изменения в setting - поставил русский язык (админка на русском). изменил rout в urls и url пути в нём. написал докстрины. заработал рейтинг. добавил валидатор на дату призведения.

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,15 +6,15 @@ from core.models import Title
 class TitlesFilter(filters.FilterSet):
     category = filters.CharFilter(
         field_name="category__slag",
-        lookup_expr="icontains"
+        lookup_expr="icontains",
     )
     name = filters.CharFilter(
         field_name="name",
-        lookup_expr="icontains"
+        lookup_expr="icontains",
     )
     genre = filters.CharFilter(
         field_name="genre__slug",
-        lookup_expr="icontains"
+        lookup_expr="icontains",
     )
 
 class Meta:
